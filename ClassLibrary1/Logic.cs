4b00ea083b4bb7af4966a233d1cd88f1fddde2e5
Dima Bondarchuk <dimich408@gmail.com>
2018-05-20 21:44:22 +0300

    Add files via upload
    add logic and change some classes

---
+++ ClassLibrary1/Logic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Logic
+    {
+        List<Item> items = new List<Item>();
+        List<User> users = new List<User>();
+        List<Order> orders = new List<Order>();
+
+        public void Registration(string login, string password, string shortName, string phone) 
+        {
+            users.Add(new User(login,password,shortName, phone));
+        }
+
+        public void ChangeItem(Item item)
+        {
+            
+        }
+
+        public void doOrder(Item item, User user) {
+            orders.Add(new Order(45, item, user));
+        }
+
+        public void newManager(User user)
+        {
+            for(int i=0; i<users.Count;i++)
+            {
+                if (users[i].Equals(user))
+                {
+                    User newManag = new Manager(user);
+                    users[i] = newManager(user);
+                }
+            }
+            User kek = new Manager(User user);
+        }
+    }
+}
---
+++ ClassLibrary1/Users/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class User
+    {
+        protected string login;
+        protected string password;
+        protected string shortName;
+        protected string phoneNumber;
+
+        public User(string login, string password, string shortName, string phoneNumber)
+        {
+            this.login = login;
+            this.password = password;
+            this.shortName = shortName;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public string getLogin() {return login; }
+        public string getpPassword() { return password; }
+        public string getShortname() { return shortName; }
+        public string getPhoneNumber() { return phoneNumber; }
+
+    }
+}
