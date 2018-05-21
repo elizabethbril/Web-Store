2c465d1b177c1ddb781c5847d058c9e31eb56861
Vlad Brusentcev <nestea08@yandex.ru>
2018-05-21 21:02:15 +0300

    Added automapper

---
+++ ClassLibrary1/AutoMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Database;
+
+namespace ClassLibrary1
+{
+    public class AutoMapper
+    {
+        public static void Initialize()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Database.Laptop, Laptop>();
+                cfg.CreateMap<Database.Accessories, Accessories>();
+                cfg.CreateMap<Database.SmartPhone, SmartPhone>();
+                cfg.CreateMap<Database.PhotoTechique,PhotoTechique>();
+                cfg.CreateMap<Database.Tablet, Tablet>();
+                cfg.CreateMap<Database.User, User>();
+                cfg.CreateMap<Database.Manager, Manager>();
+                cfg.CreateMap<Database.Admin, Admin>();
+                cfg.CreateMap<Database.Laptop, Laptop>();
+            });
+        }
+    }
+}
